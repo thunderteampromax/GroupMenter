558c1e0c953c8d2f2d21185fd69416fa51d6f1bc
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-11-22 20:29:41 +0530

    Update misc.py

--- GroupMenter/modules/helper_funcs/misc.py
+++ GroupMenter/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Support Group", url="t.me/GroupMenterGroup")]]
+        pairs += [[EqInlineKeyboardButton("Support Group", url="t.me/thundergotechnologysupport")]]
 
     return pairs
 
