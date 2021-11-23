c4d17ff73c2b67938d5206cc9c69ce147cb0328f
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-11-23 21:04:59 +0530

    Update __musicals.py

--- GroupMenter/modules/__musicals.py
+++ GroupMenter/modules/__musicals.py
@@ -4,4 +4,5 @@ __help__ = """
  ❍ /song <song name>*:* Uploads the song in it's best quality available
  💡Ex: `/song Faded Alan Walker`
  ❍ /saavn <song name>*:* Download song from saavn.
+𝐓𝐡𝐮𝐧𝐝𝐞𝐫 𝐓𝐞𝐚𝐦 Allows You to Get Song and Hear.
 """
