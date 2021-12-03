c0be835c78428da04a20dd69a703755e0d07e989
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-12-03 13:11:50 +0530

    Update bans.py

--- GroupMenter/modules/bans.py
+++ GroupMenter/modules/bans.py
@@ -339,7 +339,7 @@ def unban(update: Update, context: CallbackContext) -> str:
         return log_message
 
     chat.unban_member(user_id)
-    message.reply_text("Yep, this user can join!")
+    message.reply_text("User had been Unbanned Now the User Can Join")
 
     log = (
         f"<b>{html.escape(chat.title)}:</b>\n"
