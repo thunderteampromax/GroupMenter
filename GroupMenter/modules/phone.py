0d1b0fa4f117aa302ffc3ebee6956ba43a352dc2
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-12-05 19:47:16 +0530

    Update phone.py

--- GroupMenter/modules/phone.py
+++ GroupMenter/modules/phone.py
@@ -1,19 +1,65 @@
 import json
 
+# Copyright (C) 2021 Dihan Official
+
+
+# This file is part of Sophia (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+
+
 import requests
-from telegram.ext import CommandHandler, run_async
 
-from GroupMenter import dispatcher
-from GroupMenter.modules.helper_funcs.alternate import send_message
-from GroupMenter.modules.helper_funcs.chat_status import user_admin
+from GroupMenter import telethn as client
+from GroupMenter.events import register
 
 
-@run_async
-@user_admin
-def phone(update, context):
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
 
-    args = update.effective_message.text.split(None, 1)
-    information = args[1]
+        return isinstance(
+            (
+                await client(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (
+            await client(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    else:
+        return None
+
+# Join Our Channel @SophiaUpdates
+
+@register(pattern=r"^/phone (.*)")
+async def phone(event):
+    if event.is_group:
+        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+            await event.reply("You are not admin")
+            return
+    information = event.pattern_match.group(1)
     number = information
     key = "fe65b94e78fc2e3234c1c6ed1b771abd"
     api = (
@@ -40,13 +86,13 @@ def phone(update, context):
     e = "Carrier: " + str(carrier)
     f = "Device: " + str(line_type)
     g = f"{aa}\n{a}\n{b}\n{c}\n{d}\n{e}\n{f}"
-    send_message(update.effective_message, g)
+    await event.reply(g)
 
 
-PHONE_HANDLER = CommandHandler("phone", phone)
+__help__ = """
+*Commands:*
+• `/phone <no>`*:* find phone number details.
 
-dispatcher.add_handler(PHONE_HANDLER)
+"""
 
-
-__mod_name__ = ["PHONE"]
-__handlers__ = [PHONE_HANDLER]
+__mod_name__ = "PHONE"
